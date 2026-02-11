d7979cdfe4829745f0ee271a25de1336d1fa0171
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-11 16:53:18 +0530

    changes added

---
+++ Program4.java
@@ -0,0 +1,11 @@
+class A {
+    public static void main(String[] args) {
+        System.out.println("A main");
+    }
+}
+class B {
+    public static void main(String[] args) {
+        System.out.println("B main");
+        A.main(new String[0]);
+    }
+}
