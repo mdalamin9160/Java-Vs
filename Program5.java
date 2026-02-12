cc14907a0546b2f8832870305771a7fb6918b305
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-12 22:32:37 +0530

    all

--- Program5.java
+++ Program5.java
@@ -2,5 +2,7 @@ class Array {
     public static void main(String[] args){
         String name = "Alamin";
         System.out.println(name instanceof String);  
+        System.out.println();
+        
     }
 }
