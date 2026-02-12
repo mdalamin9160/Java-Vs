cc156c8864b39425726d7993a7d19e3404b6f3af
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-12 12:04:49 +0530

    Array

--- Program4.java
+++ Program4.java
@@ -1,5 +1,8 @@
-class A {
+class Array {
     public static void main(String[] args) {
-        System.out.println("A main");
+        int[] a = {1,5,8,9,3,2};
+        for(int i = 0; i<6; i++) {
+            System.err.println(a[i]);
+        }
     }
 }
