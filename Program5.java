5124d07c2789a6af34f716bbde82206704e15888
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-12 16:38:04 +0530

    all

--- Program5.java
+++ Program5.java
@@ -2,10 +2,9 @@ class Array {
     public static void main(String[] args){
         int a = 10;
         int b = 3;
-        System.out.println(a + b);  // 13
-        System.out.println(a - b);  // 7
-        System.out.println(a * b);  // 30
-        System.out.println(a / b);  // 3 (integer division)
-        System.out.println(a % b);  // 1git 
+        System.out.println(a > b);   // true
+        System.out.println(a == b);  // false
+        System.out.println(a != b);  // true
+
     }
 }
