2b47bbbb49f25c8c8963feb07d3fd0e0964b781c
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-13 16:31:31 +0530

    hundred

--- Program4.java
+++ Program4.java
@@ -1,9 +1,12 @@
 class name {
     public static void main(String[] args) {
-        int a = 1;
-        while(a < 1000) {
-            System.out.println(a);
-            a++;
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
         }
+        while (i <= 5);
+        System.out.println(i);
+
     }
 }
