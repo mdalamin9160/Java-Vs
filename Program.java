91afbcb85ef7d7bd09c1e0e3f72eccd6fe8a1512
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-15 10:18:28 +0530

    all

--- Program.java
+++ Program.java
@@ -1,7 +1,12 @@
-public class Program {
+class Program {
     public static void main(String[] args) {
-        Program2 obj = new Program2();
-        obj.greet();
-        System.out.println("Hello, World!");
+        int i = 1;
+        while(i<=5){
+            if(i==4){
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
     }
 }
