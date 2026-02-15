d86a5e44c6c28bb6ed81620c83d777b3e59908fe
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-15 10:23:41 +0530

    chnage

--- Program2.java
+++ Program2.java
@@ -1,10 +1,15 @@
-class Program2 {
-
-    public void greet(){
-        System.out.println("Hello shireen❤️");
-    }
+class all {
     public static void main(String[] args) {
-        System.out.println("Test Main");
-       
+        int age = 20;
+        int i = 0;
+        if(age >= 18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+        for(i=0;i<5;i++){
+            System.out.println(i);
+        }
     }
 }
