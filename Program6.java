b2ed1d9658bfbaf64bb579ff5c14aae905a4dd73
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-16 15:52:15 +0530

    OOPS

--- Program6.java
+++ Program6.java
@@ -1,8 +1,17 @@
-class name1 {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("Java");
-        sb.append(" Programming");
-        System.out.println(sb);
+class Geeks{
+    String name;
+    int id;
+    Geeks(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
     }
-
+    void display(){
+        System.out.println("GeekName: " + name
+                + " and GeekId: " + id);
+    }
+    public static void main(String[] args){
+        Geeks geek1 = new Geeks("Sweta", 68);
+        geek1.display();
+    }
 }
