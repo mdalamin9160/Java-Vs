5459f2e35f3faf2bef6eaa5e01c644c969a0aaf4
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-18 11:09:28 +0530

    All path

--- Program6.java
+++ Program6.java
@@ -1,60 +1,58 @@
 import java.util.*;
-class DFS {
+//For Youtube Lecture
+class PrintAllPaths {
     static class Edge {
         int src;
         int dest;
-        int wt;
-        public Edge(int s, int d, int w) {
+        public Edge(int s, int d) {
             this.src = s;
             this.dest = d;
-            this.wt = w;
         }
     }
     static void createGraph(ArrayList<Edge> graph[]) {
         for(int i=0; i<graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        graph[0].add(new Edge(0, 1, 1));
-        graph[0].add(new Edge(0, 2, 1));
-        graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 3, 1));
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
 
-        graph[2].add(new Edge(2, 0, 1));
-        graph[2].add(new Edge(2, 4, 1));
-        graph[3].add(new Edge(3, 1, 1));
-        graph[3].add(new Edge(3, 4, 1));
-        graph[3].add(new Edge(3, 5, 1));
-        graph[4].add(new Edge(4, 2, 1));
-        graph[4].add(new Edge(4, 3, 1));
-        graph[4].add(new Edge(4, 5, 1));
-        graph[5].add(new Edge(5, 3, 1));
-        graph[5].add(new Edge(5, 4, 1));
-        graph[5].add(new Edge(5, 6, 1));
-        graph[5].add(new Edge(6, 5, 1));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+        graph[6].add(new Edge(6, 5));
     }
-    public static void dfs(ArrayList<Edge> graph[], int curr, boolean visited[]) {
-        if(visited[curr]) {
+    public static void printAllPaths(ArrayList<Edge> graph[], int src, int tar, String
+            path, boolean vis[]) {
+        if(src == tar) {
+            System.out.println(path);
             return;
         }
-        System.out.print(curr+" ");
-        visited[curr] = true;
-        for(int i=0; i<graph[curr].size(); i++) {
-            Edge e = graph[curr].get(i);
-            dfs(graph, e.dest, visited);
+        for(int i=0; i<graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if(!vis[e.dest]) {
+                vis[e.dest] = true;
+                printAllPaths(graph, e.dest, tar, path+"->"+e.dest, vis);
+                vis[e.dest] = false;
+            }
         }
     }
     public static void main(String args[]) {
-/*
-1 --- 3
-/ | \
-0 | 5 -- 6
-\ | /
-2 ---- 4
-*/
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        boolean visited[] = new boolean[V];
-        dfs(graph, 0, visited);
+        int src = 0;
+        int tar = 5;
+        boolean vis[] = new boolean[V];
+
     }
 }
