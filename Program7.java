6c459d431f19d1df114c235a90581132055ea91e
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-20 16:49:26 +0530

    exception

--- Program7.java
+++ Program7.java
@@ -1,14 +1,15 @@
 public class Program7 {
+
+    public static void checkAge(int age) {
+
+        if (age < 18) {
+            throw new ArithmeticException("Not eligible");
+        }
+
+        System.out.println("Eligible");
+    }
+
     public static void main(String[] args) {
-
-        System.out.println("Program started");
-
-        int a = 10;
-        int b = 5;
-        int result = a + b;
-
-        System.out.println("Result: " + result);
-
-        System.out.println("Program ended normally");
+        checkAge(15);
     }
 }
