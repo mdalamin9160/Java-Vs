da6df94733d2a803fc9022f489ae931c44166201
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-21 14:19:08 +0530

    final

--- Program7.java
+++ Program7.java
@@ -1,18 +1,6 @@
-class Student {
-    String name;
-    static String school = "ABC School";
-    Student(String name){
-        this.name = name;
-    }
-    void display(){
-        System.out.println(name + " studies at " + school);
-    }
-}
-public class Program7 {
+class age {
     public static void main(String[] args) {
-        Student s1 = new Student("Alice");
-        Student s2 = new Student("Bob");
-        s1.display();
-        s2.display();
+        final int age = 20;
+        System.out.println(age);
     }
 }
