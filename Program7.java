6e27504e23f8d5a65d1c8b34f7882a5050d38b17
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-22 11:20:34 +0530

    arraylist

--- Program7.java
+++ Program7.java
@@ -1,6 +1,36 @@
-class pi {
-    static final double pi = 3.14;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Program7 {
     public static void main(String[] args) {
-        System.out.println(pi);
+        ArrayList<Integer> list = new ArrayList<>();
+        //add elements to the list
+        list.add(0);
+        list.add(2);
+        list.add(1);
+        System.out.println(list);
+        //get element
+        int element = list.get(0);
+        System.out.println(element);
+        //add element in between
+        list.add(0, 9);
+        System.out.println(list);
+        //set element
+        list.set(1, 5);
+        System.out.println(list);
+        //remove element
+        list.remove(2);
+        System.out.println(list);
+        //size of the list
+        int size = list.size();
+        System.out.println(size);
+        //loops
+        for(int i = 0; i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        System.out.println();
+        //sort the list
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
