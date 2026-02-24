7e9c36175e1106db8bd4b50ae8298bc8905a80c8
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-24 14:35:49 +0530

    name

---
+++ All.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class All{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = sc.nextLine();
+        System.out.println("Name: "+name);
+
+        
+    }
+}
