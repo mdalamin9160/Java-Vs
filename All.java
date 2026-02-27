23aa0c480ee7ba15930838fd0525bed95af1cd8b
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-27 14:38:38 +0530

    all2

--- All.java
+++ All.java
@@ -5,7 +5,9 @@ import java.io.InputStreamReader;
 public class All {
     public static void main(String[] args) throws IOException {
 
-
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
         @Override
         System.out.println("Enter name: ");
         String name = br.readLine();
