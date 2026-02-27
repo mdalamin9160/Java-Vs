a9ddb95a31559169056860031edf561be005853a
mdalamin9160 <its.alamin.12@gmail.com>
2026-02-27 14:37:49 +0530

    all

--- All.java
+++ All.java
@@ -1,14 +1,21 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-public class All{
-    public static void main(String[] args) {
-    BufferedReader  br = new BufferedReader(new InputStreamReader((System.in)));
-    System.out.println("Enter name: ");
-    String name = br.readLine();
-    System.out.println("Enter age");
-    int age = Integer.parseInt(br.readLine());
-    System.out.println(name);
-    System.out.println(age);
+
+public class All {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
+        @Override
+        System.out.println("Enter name: ");
+        String name = br.readLine();
+
+        System.out.println("Enter age: ");
+        int age = Integer.parseInt(br.readLine());
+
+        System.out.println(name);
+        System.out.println(age);
     }
 }
